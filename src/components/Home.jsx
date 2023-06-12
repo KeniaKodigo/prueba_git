1577dc3d80b090b85a979b4f7876b6483a8a4b8d
KeniaKodigo <kenia.paz@kodigo.org>
2023-06-12 17:11:16 -0600

    cambiando de texto

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+        <section>
+            <h2>estamos en git</h2>
+        </section>
+    </div>
   )
 }
